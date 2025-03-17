f0ed231099c31aa2b100520667da6312ba0a1e06
Ziad-Asr <ziadelsayedasr@gmail.com>
2025-03-17 17:57:26 +0200

    removing broke components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import Slider3d from "./Slider3d/Slider3d";
-import AirpodsSlider from "./AirpodsSlider/AirpodsSlider";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      {/* <Slider3d /> */}
-
-      <AirpodsSlider />
+      <Slider3d />
     </div>
   );
 }
